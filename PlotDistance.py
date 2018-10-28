28dc16790d1c656422bc050dfb1780303b22c820
NEO <NEOissss@users.noreply.github.com>
2018-10-28 15:55:24 -0400

    fix

--- PlotDistance.py
+++ PlotDistance.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,10 +25,15 @@ def plot_distance(filename):
     x0 = [i for i, j in enumerate(mask) if not j]
     pos1 = [pos[i] for i, j in enumerate(mask) if j]
     pos0 = [pos[i] for i, j in enumerate(mask) if not j]
-    plt.scatter(x1, pos1, c='g', alpha=0.2, label='Positive')
-    plt.scatter(x0, pos0, c='r', alpha=0.2, label='False Negative')
     for i in range(9):
         plt.scatter(x, neg[:, i], c='b', alpha=0.2, label='Negative')
+    plt.scatter(x1, pos1, c='g', alpha=0.5, label='Positive')
+    plt.scatter(x0, pos0, c='r', alpha=0.5, label='False Negative')
     plt.xlabel('#Case')
     plt.ylabel('Distance')
     plt.savefig('{:s}/{:s}'.format(path, name))
+
+
+if __name__ == '__main__':
+    for i in sys.argv[1:]:
+        plot_distance(i)
