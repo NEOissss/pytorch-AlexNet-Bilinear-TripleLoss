9e7e8625ae1b861b97f4b20745225874ab9e30ce
NEO <NEOissss@users.noreply.github.com>
2018-10-29 01:59:40 -0400

    add bar plot

--- PlotDistance.py
+++ PlotDistance.py
@@ -7,9 +7,9 @@ import numpy as np
 def plot_distance(filename):
     if os.path.isdir(filename):
         path = filename
-        for j in os.listdir(i):
+        for j in os.listdir(path):
             if 'test_result' in j:
-                metric = np.load(j)
+                metric = np.load(path + '/' + j)
                 name = j.split('.')[0]
                 break
     else:
@@ -40,6 +40,45 @@ def plot_distance(filename):
     plt.savefig('{:s}/{:s}'.format(path, name))
 
 
+def plot_distance_imporovement(filename):
+    if os.path.isdir(filename):
+        path = filename
+        for j in os.listdir(path):
+            if 'test_result' in j:
+                metric = np.load(path + '/' + j)
+                name = j.split('.')[0]
+                break
+    else:
+        path = '.'
+        metric = np.load(filename)
+        name = filename.split('.')[0]
+
+    baseline = np.load('baseline/baseline_result_eval_trans_test.npy')
+    baseline_rank = (baseline[:, 0] < baseline[:, 1:]).sum(1)
+    metric_rank = (metric[:, 0] < metric[:, 1:]).sum(1)
+
+    y = metric_rank - baseline_rank
+    k = metric.shape[0]
+    # 1: False->True, 2: True->False, 3: Increase 4: Decrease
+    x1 = [i for i in range(k) if metric_rank[i]==9 and y[i]>0]
+    y1 = [y[i] for i in range(k) if metric_rank[i]==9 and y[i]>0]
+    x2 = [i for i in range(k) if baseline_rank[i]==9 and y[i]<0]
+    y2 = [y[i] for i in range(k) if baseline_rank[i]==9 and y[i]<0]
+    x3 = [i for i in range(k) if metric_rank[i]!=9 and y[i]>0]
+    y3 = [y[i] for i in range(k) if metric_rank[i]!=9 and y[i]>0]
+    x4 = [i for i in range(k) if baseline_rank[i]!=9 and y[i]<0]
+    y4 = [y[i] for i in range(k) if baseline_rank[i]!=9 and y[i]<0]
+
+    plt.bar(x1, y1, color='g', linewidth=0)
+    plt.bar(x2, y2, color='r', linewidth=0)
+    plt.bar(x3, y3, color='b', linewidth=0)
+    plt.bar(x4, y5, color='m', linewidth=0)
+    plt.xlabel('#Case')
+    plt.ylabel('Rank')
+    plt.savefig('{:s}/{:s}_bar'.format(path, name))
+
+
 if __name__ == '__main__':
     for i in sys.argv[1:]:
         plot_distance(i)
+        plot_distance_imporovement(i)
